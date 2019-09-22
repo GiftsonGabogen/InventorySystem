f066b4800875e17322ab760d21804d4f2408a6b2
unknown <ebbengabogen@gmail.com>
2019-09-23 01:36:52 +0800

    Added a get method on server side which fetch the sales on a certain month, fetch sales, identify items with the same name, add all its quantity.Example:he want to fetch the month of september [{Name:Ball,Month:september,Quantity:7},{Name:book,Month:september,Quantity:2},{Name:Ball,Month:november,Quantity:3},{Name:Ball,Month:september,Quantity:1}], the output will be [{Name:Ball,Month:september,Quantity:8},{Name:book,Month:september,Quantity:2}]... While I'm Doing This, I Also Realized That, Now I Know Why Big Tech Companies(FANG) do interview with data structure and algorithm, this method saves ton of data. with just sorting and filtering it on computer, I can achieve what I want even without using algorithms,when a new sale is made, I can just add a new collections that say salesInMonth and add the quantities on the respective item that made sale on that month but the downside is If Im a big company, Data Will Be Bigger on Database Because of The Extra Collection, So it will not be efficient. Now I Know How Important Algorithm and Data Structure is, Just Filter it on server side but the downside is the response will be slower but the difference is not that significant and also even it is a 100,000 data you will not load it all anyway because there will be pagination and the more important thing is you saved a lot of data, by the way, this code made me 2 hours to make and debug it may be slow compared to other developer and if it is actually a coding interview but hey, I'm proud I did it. woooooh, Now I Know, Fuck Yeah, Feels Good to use algorithm on a real life scenario

--- Routes/sales.js
+++ Routes/sales.js
@@ -32,8 +32,8 @@ Router.get("/:id", (req, res) => {
     id
   } = req.params;
   Sales.find({
-      _id: id
-    })
+    _id: id
+  })
     .populate("Name")
     .exec()
     .then(result => {
@@ -57,6 +57,75 @@ Router.get("/:id", (req, res) => {
     });
 });
 
+Router.get("/bymonth/:month", (req, res) => {
+  const { month } = req.params
+  Sales.find()
+    .exec()
+    .then(result => {
+      let sales = result.filter(sale => moment(sale.Date).format("MMM") === month)
+
+      // Scanning all the Sales Array and Adding The Same Item
+      const ScanningandMerging = (ItemsToScan) => {
+        //the Array That we will Respond to the client when the filtering is done
+        const filteredSales = []
+        function recursiveScanning(array_to_be_scan) {
+          // if the array has no equal name and it is the only one left on an array
+          // push it to the filteredSales array
+          if (array_to_be_scan.length === 1) {
+            filteredSales.push(array_to_be_scan[0])
+            recursiveScanning([])
+            // if there is no already items on the array finish the scanning and respond to the client
+            // with the filteredSales
+          } else if (array_to_be_scan.length === 0) {
+            res.status(200).json({
+              success: true,
+              Sales: filteredSales
+            });
+          } else {
+            let Items = []
+            let newArray = array_to_be_scan.filter(array => 1 === 1)
+            let j = 1;
+            for (let i = 0; i < array_to_be_scan.length - 1; i++) {
+              let temp1 = array_to_be_scan[0].ItemName;
+              let temp2 = array_to_be_scan[i + 1].ItemName
+              // if the first and i+1 index has the same name push the Quantity data to a temp array
+              // remove it to the original array so that on the next recursiveScanning the scanned item will not be scanned again
+              if (temp1 === temp2) {
+                Items.push(array_to_be_scan[i + 1].Quantity)
+                newArray.splice(i + j, 1)
+                // we are reducing the arrays length so we need to reduce also the index that we splice
+                j--
+              }
+              // if it is the last item to be compared of from the first index
+              // add all the quantities
+              // remove the first index because it was finish scanning
+              // then the remaining items on the array will be scanned again on the recursive Scanning
+              if (i >= array_to_be_scan.length - 2) {
+                var Quantity = newArray[0].Quantity;
+                for (let x = 0; x < Items.length; x++) {
+                  Quantity = Number(Quantity) + Number(Items[x])
+                }
+                newArray[0].Quantity = Quantity
+                filteredSales.push(newArray[0])
+                newArray.splice(0, 1)
+                recursiveScanning(newArray)
+              }
+            }
+          }
+
+        }
+        recursiveScanning(ItemsToScan)
+      }
+      ScanningandMerging(sales)
+    })
+    .catch(err => {
+      res.status(200).json({
+        sucess: false,
+        message: err
+      });
+    });
+});
+
 Router.post("/", AuthCheck, (req, res) => {
   const {
     ItemName,
@@ -68,15 +137,15 @@ Router.post("/", AuthCheck, (req, res) => {
 
   let Category;
   Items.find({
-      _id: ItemID
-    })
+    _id: ItemID
+  })
     .populate("Category")
     .exec()
     .then(result => {
       // reduce the Sold Item
       Items.findOneAndUpdate({
-          _id: ItemID
-        }, {
+        _id: ItemID
+      }, {
           $set: {
             Quantity: Number(result[0].Quantity) - Number(Quantity),
 
@@ -95,8 +164,8 @@ Router.post("/", AuthCheck, (req, res) => {
         Category
       });
       Sales.find({
-          ItemID: ItemID
-        })
+        ItemID: ItemID
+      })
         .exec()
         .then(saleRes => {
           if (saleRes.length > 0) {
@@ -108,8 +177,8 @@ Router.post("/", AuthCheck, (req, res) => {
             // else create new sale on this day or create new sale with the new seller
             if (fetchDate === updateDate && saleRes[0].Seller === Seller) {
               Sales.findOneAndUpdate({
-                  _id: saleRes[0]._id
-                }, {
+                _id: saleRes[0]._id
+              }, {
                   $set: {
                     Quantity: Number(saleRes[0].Quantity) + Number(Quantity),
                   }
@@ -158,8 +227,8 @@ Router.delete("/:id", AuthCheck, (req, res) => {
     id
   } = req.params;
   Sales.findOneAndDelete({
-      _id: id
-    })
+    _id: id
+  })
     .exec()
     .then(result => {
       res.status(201).json({
