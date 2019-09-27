15e126a0147e94e8702dd31b27c81c8959844616
unknown <ebbengabogen@gmail.com>
2019-09-28 00:31:19 +0800

    recode the routes api/sales/bymonth using better algorithm

--- Routes/sales.js
+++ Routes/sales.js
@@ -65,59 +65,24 @@ Router.get("/bymonth/:month", (req, res) => {
       let sales = result.filter(
         sale => moment(sale.Date).format("MMM YYYY") === month
       );
-
-      // Scanning all the Sales Array and Adding The Same Item
-      const ScanningandMerging = ItemsToScan => {
-        //the Array That we will Respond to the client when the filtering is done
-        const filteredSales = [];
-        function recursiveScanning(array_to_be_scan) {
-          // if the array has no equal name and it is the only one left on an array
-          // push it to the filteredSales array
-          if (array_to_be_scan.length === 1) {
-            filteredSales.push(array_to_be_scan[0]);
-            recursiveScanning([]);
-            // if there is no already items on the array finish the scanning and respond to the client
-            // with the filteredSales
-          } else if (array_to_be_scan.length === 0) {
-            res.status(200).json({
-              success: true,
-              Sales: filteredSales
-            });
-          } else {
-            let Items = [];
-            let newArray = array_to_be_scan.filter(array => 1 === 1);
-            let j = 1;
-            for (let i = 0; i < array_to_be_scan.length - 1; i++) {
-              let temp1 = array_to_be_scan[0].ItemName;
-              let temp2 = array_to_be_scan[i + 1].ItemName;
-              // if the first and i+1 index has the same name push the Quantity data to a temp array
-              // remove it to the original array so that on the next recursiveScanning the scanned item will not be scanned again
-              if (temp1 === temp2) {
-                Items.push(array_to_be_scan[i + 1].Quantity);
-                newArray.splice(i + j, 1);
-                // we are reducing the arrays length so we need to reduce also the index that we splice
-                j--;
-              }
-              // if it is the last item to be compared of from the first index
-              // add all the quantities
-              // remove the first index because it was finish scanning
-              // then the remaining items on the array will be scanned again on the recursive Scanning
-              if (i >= array_to_be_scan.length - 2) {
-                var Quantity = newArray[0].Quantity;
-                for (let x = 0; x < Items.length; x++) {
-                  Quantity = Number(Quantity) + Number(Items[x]);
-                }
-                newArray[0].Quantity = Quantity;
-                filteredSales.push(newArray[0]);
-                newArray.splice(0, 1);
-                recursiveScanning(newArray);
-              }
-            }
+      let newSales = [];
+      for (x of sales) {
+        let found = false;
+        for (y of newSales) {
+          if (y.ItemName === x.ItemName) {
+            y.Quantity = x.Quantity + y.Quantity;
+            found = true;
           }
         }
-        recursiveScanning(ItemsToScan);
-      };
-      ScanningandMerging(sales);
+        if (found === false) {
+          newSales = [...newSales, x];
+        }
+      }
+      res.status(200).json({
+        message: "Fetch Successfully",
+        success: true,
+        Sales: newSales
+      });
     })
     .catch(err => {
       res.status(200).json({
@@ -137,59 +102,24 @@ Router.get("/byday/:day", (req, res) => {
       let sales = result.filter(
         sale => moment(sale.Date).format("MMM YYYY D") === day
       );
-
-      // Scanning all the Sales Array and Adding The Same Item
-      const ScanningandMerging = ItemsToScan => {
-        //the Array That we will Respond to the client when the filtering is done
-        const filteredSales = [];
-        function recursiveScanning(array_to_be_scan) {
-          // if the array has no equal name and it is the only one left on an array
-          // push it to the filteredSales array
-          if (array_to_be_scan.length === 1) {
-            filteredSales.push(array_to_be_scan[0]);
-            recursiveScanning([]);
-            // if there is no already items on the array finish the scanning and respond to the client
-            // with the filteredSales
-          } else if (array_to_be_scan.length === 0) {
-            res.status(200).json({
-              success: true,
-              Sales: filteredSales
-            });
-          } else {
-            let Items = [];
-            let newArray = array_to_be_scan.filter(array => 1 === 1);
-            let j = 1;
-            for (let i = 0; i < array_to_be_scan.length - 1; i++) {
-              let temp1 = array_to_be_scan[0].ItemName;
-              let temp2 = array_to_be_scan[i + 1].ItemName;
-              // if the first and i+1 index has the same name push the Quantity data to a temp array
-              // remove it to the original array so that on the next recursiveScanning the scanned item will not be scanned again
-              if (temp1 === temp2) {
-                Items.push(array_to_be_scan[i + 1].Quantity);
-                newArray.splice(i + j, 1);
-                // we are reducing the arrays length so we need to reduce also the index that we splice
-                j--;
-              }
-              // if it is the last item to be compared of from the first index
-              // add all the quantities
-              // remove the first index because it was finish scanning
-              // then the remaining items on the array will be scanned again on the recursive Scanning
-              if (i >= array_to_be_scan.length - 2) {
-                var Quantity = newArray[0].Quantity;
-                for (let x = 0; x < Items.length; x++) {
-                  Quantity = Number(Quantity) + Number(Items[x]);
-                }
-                newArray[0].Quantity = Quantity;
-                filteredSales.push(newArray[0]);
-                newArray.splice(0, 1);
-                recursiveScanning(newArray);
-              }
-            }
+      let newSales = [];
+      for (x of sales) {
+        let found = false;
+        for (y of newSales) {
+          if (y.ItemName === x.ItemName) {
+            y.Quantity = x.Quantity + y.Quantity;
+            found = true;
           }
         }
-        recursiveScanning(ItemsToScan);
-      };
-      ScanningandMerging(sales);
+        if (found === false) {
+          newSales = [...newSales, x];
+        }
+      }
+      res.status(200).json({
+        message: "Fetch Successfully",
+        success: true,
+        Sales: newSales
+      });
     })
     .catch(err => {
       res.status(200).json({
